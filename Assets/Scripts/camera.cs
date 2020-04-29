8a567f46b5c8c397dc7f7d7c89301a4476a8763f
Jose Hamilton <josehamiltonmartinsleite@gmail.com>
2020-04-28 22:28:42 -0300

    This is my game finished

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject corona;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.y = corona.transform.position.y + 3;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/corona.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class corona : MonoBehaviour
+{
+    public AudioSource som1;
+    public AudioSource som2;
+
+    public Text txtVidas;
+    private static int vidas = 5;
+
+    void Start() 
+    {
+        txtVidas.text = "Vidas: " + vidas;
+
+        som1 = GetComponents<AudioSource>()[0];
+        som2 = GetComponents<AudioSource>()[1];
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        som1.Play();
+    }
+
+    void OnTriggerEnter(Collider outro) 
+    {
+        if (outro.gameObject.CompareTag("vermelho")) {
+            vidas--;
+            txtVidas.text = "Vidas: " + vidas;
+
+            som2.Play();
+            InvokeRepeating("perder", 0.5f, 1);
+        }
+    }
+
+    void perder()
+    {
+        if (vidas < 1)
+        {
+            SceneManager.LoadScene("gameover");
+        }
+        else
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
